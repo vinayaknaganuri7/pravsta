53c30ac86a413c071bbdb44467bd4499db39e11e
vinayaknaganuri7 <vinayaknaganuri7@gmail.com>
2025-07-23 17:32:49 +0530

    Initial commit - added MObile size menu bar create  project files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,17 +29,48 @@ const Navbar = () => {
         setIsServicesOpen(false);
       }
     };
+
     document.addEventListener('mousedown', handleClickOutside);
-    return () => document.removeEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
   }, []);
 
   return (
     <nav className="navbar" ref={navRef}>
-      <div className="navbar-top">
+      <div className="navbar-left">
         <Link to="/" className="logo-link" onClick={handleLinkClick}>
           <img src={logo} alt="Logo" className="logo-image" />
           <span className="company-name">PRAVSTA TECHNOLOGY PRIVATE LIMITED</span>
         </Link>
+      </div>
+
+      <div className="navbar-right">
+        <ul className={`navbar-links ${isMenuOpen ? 'active' : ''}`}>
+          <li><Link to="/" className="navbar-link" onClick={handleLinkClick}>Home</Link></li>
+          <li><Link to="/about" className="navbar-link" onClick={handleLinkClick}>About Us</Link></li>
+
+          <li
+            className="dropdown"
+            onMouseEnter={() => window.innerWidth > 768 && setIsServicesOpen(true)}
+            onMouseLeave={() => window.innerWidth > 768 && setIsServicesOpen(false)}
+          >
+            <span className="navbar-link" onClick={toggleServicesMobile}>
+              Services ▾
+            </span>
+            {isServicesOpen && (
+              <ul className="dropdown-menu">
+                <li><Link to="/services/automotive" onClick={handleLinkClick}>Automotive Services</Link></li>
+                <li><Link to="/services/ai-data" onClick={handleLinkClick}>AI & Data Services</Link></li>
+                <li><Link to="/services/sap" onClick={handleLinkClick}>SAP Services</Link></li>
+                <li><Link to="/services/web-mobile" onClick={handleLinkClick}>Web & Mobile Development</Link></li>
+              </ul>
+            )}
+          </li>
+
+          <li><Link to="/contact" className="navbar-link" onClick={handleLinkClick}>Contact</Link></li>
+          <li><button className="cta-button" onClick={handleLinkClick}>Get in Touch</button></li>
+        </ul>
 
         <div className="navbar-hamburger" onClick={toggleMenu}>
           <div className="bar"></div>
@@ -47,30 +78,6 @@ const Navbar = () => {
           <div className="bar"></div>
         </div>
       </div>
-
-      <ul className={`navbar-menu ${isMenuOpen ? 'active' : ''}`}>
-        <li><Link to="/" className="navbar-link" onClick={handleLinkClick}>Home</Link></li>
-        <li><Link to="/about" className="navbar-link" onClick={handleLinkClick}>About Us</Link></li>
-        <li
-          className="dropdown"
-          onMouseEnter={() => window.innerWidth > 768 && setIsServicesOpen(true)}
-          onMouseLeave={() => window.innerWidth > 768 && setIsServicesOpen(false)}
-        >
-          <span className="navbar-link" onClick={toggleServicesMobile}>
-            Services ▾
-          </span>
-          {isServicesOpen && (
-            <ul className="dropdown-menu">
-              <li><Link to="/services/automotive" onClick={handleLinkClick}>Automotive Services</Link></li>
-              <li><Link to="/services/ai-data" onClick={handleLinkClick}>AI & Data Services</Link></li>
-              <li><Link to="/services/sap" onClick={handleLinkClick}>SAP Services</Link></li>
-              <li><Link to="/services/web-mobile" onClick={handleLinkClick}>Web & Mobile Development</Link></li>
-            </ul>
-          )}
-        </li>
-        <li><Link to="/contact" className="navbar-link" onClick={handleLinkClick}>Contact</Link></li>
-        <li><button className="cta-button" onClick={handleLinkClick}>Get in Touch</button></li>
-      </ul>
     </nav>
   );
 };
